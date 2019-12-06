f75a5fc635249028e1ce974c5db921d657b0382d
Shawn <shawncolby@billericak12.com>
2019-12-06 10:44:33 -0500

    fgjdsiksjryik

--- src/com/company/Exercises.java
+++ src/com/company/Exercises.java
@@ -51,19 +51,35 @@ public class Exercises {
 
     public static void dollarsAndStars() {
         int b = 0;
+        int e = 0;
+        int g = 0;
+        int j = 0;
+        int l = 0;
         for (int a = 1; a <= 7; a++) {
             for (int c = 0; c < b; c++) {
                 System.out.print("*");
             }
             b = b + 2;
-            for (int d=7; d<b; d--){
-                System.out
+            for (int d=7; d>e; d--){
+                System.out.print("$");
             }
+            e = e + 1;
+            for (int f=14; f>g; f--){
+                System.out.print("*");
+            }
+            g = g + 2;
+            for (int h=7; h>j; h--){
+                System.out.print("$");
+            }
+            j = j + 1;
+            for (int k = 0; k < l; k++) {
+                System.out.print("*");
+            }
+            l = l + 2;
             System.out.println();
         }
     }
-
-
+    
     public static void main (String [] args){
         System.out.println(addOdds(9));
         System.out.println(addOdds(10));
